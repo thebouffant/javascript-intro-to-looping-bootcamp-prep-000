796963d75e6907da636da5ef00e41b6287bf0af0
Marc <marc.troughton@gmail.com>
2018-12-26 11:01:45 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,40 @@
+
+function forLoop(name){
+ for (let i = 0; i < 25; i++) {
+  if (i === 1) {
+ 
+  name.push("I am 1 strange loop.")
+ 
+  } else {
+  
+  name.push(`I am ${i} strange loops.`)
+  }
+}
+  return name
+}
+
+function whileLoop(number){
+  while (number > 0) {
+  console.log(--number);
+  }
+  return "done"
+}
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(name){
+  
+  do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
+  
+}
+
+
+
+
